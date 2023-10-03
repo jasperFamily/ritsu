22852811f7e494d48d315ab877954b87474a994e
USER <USER@LAPTOP-T2IDA7T9>
2023-10-04 07:24:04 +0900

    Merge branch 'master' of https://github.com/jasperFamily/ritsu.git

--- src/ritsu/Test.java
+++ src/ritsu/Test.java
@@ -1,8 +1,13 @@
 package ritsu;
 
+import java.util.Scanner;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("a");
+		Scanner scan = new Scanner(System.in);
+		System.out.println(scan);
+		int num1 = scan.nextInt();
 	}
 
 }
