bc8de35ed9c4b1a501b353caa0a3c47139ebae4b
shige <shige@jasper>
2023-10-04 07:31:02 +0900

    a

--- src/ritsu/Denntaku.java
+++ src/ritsu/Denntaku.java
@@ -5,11 +5,11 @@ public class Denntaku {
 	public static void main(String[] args) {
 		System.out.println("電卓");
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("数字を２こ入力してください");
 		int num1 = scan.nextInt();
 		int num2 = scan.nextInt();
-		
+
 		System.out.println(num1 + "+" +  num2 + " = " +( num1 + num2)) ;
 	}
 
