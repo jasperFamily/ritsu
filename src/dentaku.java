6bb49fe2199aec934726aa3829c12bc80a159568
wakaba <xxxxxxxx@xxxxx.com>
2023-10-06 17:40:25 +0900

    a

---
+++ src/dentaku.java
@@ -0,0 +1,8 @@
+
+public class dentaku {
+	public static void mano(String[] asrg) {
+		System.out.println("電卓");
+		
+	}
+
+}
--- src/kou/Kou.java
+++ src/kou/Kou.java
@@ -1,7 +1,26 @@
 package kou;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
+
 public class Kou {
-	public static void mani(String[] asrg) {
-		System.out.println(1111);
+	public static void main (String[] args) {
+		
+		Calendar c = Calendar.getInstance();
+		
+		SimpleDateFormat sdf
+		= new SimpleDateFormat("y/M/d");
+		
+		System.out.println(sdf.format(c.getTime()));
+		
+		String[] unmei = {"貴方は元気ですね","貴方は普通ですね","貴方は疲れていますね"};
+		
+		Random r = new Random();
+		
+		System.out.println(unmei[r.nextInt(3)]);
+		
 	}
+	
+		
 }
