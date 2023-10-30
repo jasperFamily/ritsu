919f4ef8404b235e106fbea7196ad78c7d62f651
wakaba <xxxxxxxx@xxxxx.com>
2023-10-30 18:30:41 +0900

    d

---
+++ src/kou/Kzu.java
@@ -0,0 +1,20 @@
+package kou;
+
+public class Kzu {
+	public static void main(String[] sarg) {
+		int num2 = 53;
+		int num5 = 43;
+		
+		System.out.println(num2 + num5);
+		System.out.println(num2 - num5);
+		System.out.println(num2 * num5);
+		System.out.println((double) num2 / num5);
+		System.out.println(num2 % num5);
+		
+		num2++;
+		
+		System.out.println(num2);
+		
+	}
+
+}
