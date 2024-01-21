38e601bae4d38ea9208de30f5586769fe7eb42cd
USER <USER@LAPTOP-T2IDA7T9>
2024-01-21 18:21:31 +0900

    a

--- src/ritsu/Fairu7.java
+++
@@ -1,6 +0,0 @@
-//STEP1 まず通常のファイル出ry九ストリームfosを作成
-FileOutputStream fos = new FileOutputStream("data.txt");
-//sutettpu
-CipherOutputStream cos = new CipherOutputStream(fos.algo);
-//stettpu
-coa.write(65);
--- src/ritsu/test01/Fairu7.java
+++
@@ -1,6 +0,0 @@
-FileReader fr = new  FileReader("rpgsave.dat");
-BufferedReader br = new BufferedReader(fr);
-String line = null;
-while ((line = br.readLine()) != null) {
-	
-}
